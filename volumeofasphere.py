0d575ef6dd65cdb0adf00b592aa6501775c0e68c
Nolan-Rodriguez <nmrodriguez@proton.me>
2025-05-24 04:59:03 -0400

    Update volumeofasphere.py

--- volumeofasphere.py
+++ volumeofasphere.py
@@ -7,4 +7,4 @@ sphere_radius = float(input("What is the radius of the sphere: "))
 volume = (4 / 3) * math.pi * sphere_radius ** 3
 
 # print volume
-print ('The volume of a sphere with a radius of {sphere_radius} is {volume})
+print (f'The volume of a sphere with a radius of {sphere_radius} is {volume}')
