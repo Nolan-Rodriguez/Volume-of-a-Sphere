c141ead8c76da7274c7ac19fe0ace3c697762e6e
Nolan-Rodriguez <nmrodriguez@proton.me>
2025-05-24 04:28:34 -0400

    Add files via upload

---
+++ volumeofasphere.py
@@ -0,0 +1,7 @@
+import math
+
+# get radius of sphere
+sphere_radius = float(input("What is the radius of the sphere: "))
+
+# calculate volume of sphere
+print ('The approximate volume of a sphere with a radius of sphere_radius is ', (4 / 3) * math.pi * sphere_radius ** 3)
